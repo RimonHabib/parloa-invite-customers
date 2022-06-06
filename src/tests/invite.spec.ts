45aa3db4ea5e3c6cc59f7677e4e33243fb84a68d
Rimon Habib <rimon@virtue5.com>
2022-06-06 22:11:40 +0600

    refactor: use testing setup with beforeach

--- src/tests/invite.spec.ts
+++ src/tests/invite.spec.ts
@@ -3,15 +3,19 @@ import DistanceCalculator from '../distance-calculator';
 import Invite from '../invite';
 
 describe('Invite', () => {
+  let invite;
+
+  beforeEach(() => {
+    invite = new Invite(new DistanceCalculator(), new CustomerDataPerser());
+  });
+
   it('Should return invite list', async () => {
-    const invite = new Invite(new DistanceCalculator(), new CustomerDataPerser());
     const list = await invite.getList();
     expect(list.length).toBeGreaterThan(1);
   });
 
   // 10k radius
   it('Should return 1 customer', async () => {
-    const invite = new Invite(new DistanceCalculator(), new CustomerDataPerser());
     const list = await invite.getList({
       radius: 10,
       min: 2,
@@ -21,7 +25,6 @@ describe('Invite', () => {
   });
 
   it('Should return 0 customer', async () => {
-    const invite = new Invite(new DistanceCalculator(), new CustomerDataPerser());
     const list = await invite.getList({
       radius: 0,
       unit: 'mile',
@@ -31,7 +34,6 @@ describe('Invite', () => {
   });
 
   it('Should return 10 customer max', async () => {
-    const invite = new Invite(new DistanceCalculator(), new CustomerDataPerser());
     const list = await invite.getList({
       radius: Infinity,
       max: 10,
@@ -40,14 +42,12 @@ describe('Invite', () => {
   });
 
   it('Should return in correct order', async () => {
-    const invite = new Invite(new DistanceCalculator(), new CustomerDataPerser());
     const acendingOrderList = await invite.getList();
     const decendingOrderList = await invite.getList({ sort: 'DESC' });
     expect(acendingOrderList.reverse()).toEqual(decendingOrderList);
   });
 
   it('Should throw an error', async () => {
-    const invite = new Invite(new DistanceCalculator(), new CustomerDataPerser());
     await expect(invite.getList({ filePath: 'WrongFilePath' })).rejects.toThrow();
   });
 });
